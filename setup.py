58cd586e684e836f6f3f75d786536d1d8883a1f4
Mori Benech <mori@trapx.com>
2020-06-16 22:59:46 +0300

    modified:   setup.py

--- setup.py
+++ setup.py
@@ -24,6 +24,11 @@ here = path.abspath(path.dirname(__file__))
 # Get the long description from the README file
 #with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 #    long_description = f.read()
+long_description = '''aioconnectors is an easy to set up broker that works on Unix like systems. Requirements are : Python >= 3.6, and openssl installed.
+It provides optional authentication and encryption, transfer of messages and files, persistence in case of connection loss.
+It is built on the client/server model but both peers can push messages. Based on asyncio, message sending and receiving are asynchronous, either independent or with the option to wait asynchronously for a response.
+A connector can be configured with a short json file. An embedded command line tool enables to easily run a connector and manage it with shell commands.
+A simple programmatic Python API is also exposed, with functionalities like starting/stopping a connector, sending a message, or receiving messages, and other management capabilities.'''
 
 # Arguments marked as "Required" below must be included for upload to PyPI.
 # Fields marked as "Optional" may be commented out.
@@ -63,7 +68,7 @@ setup(
     #
     # This field corresponds to the "Description" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#description-optional
-#    long_description=long_description,  # Optional
+    long_description=long_description,  # Optional
 
     # This should be a valid link to your project's main homepage.
     #
