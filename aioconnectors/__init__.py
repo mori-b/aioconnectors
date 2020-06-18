703351cc14b3dde6a94f87ca993ace066b60c968
Mori Benech <mori@trapx.com>
2020-06-18 10:32:12 +0300

    increment version 	modified:   aioconnectors/__init__.py 	modified:   setup.py

--- aioconnectors/__init__.py
+++ aioconnectors/__init__.py
@@ -29,7 +29,7 @@ The command line tool can be called by "python3 -m aioconnectors --help"
 Usage examples can be found in applications.py
 '''
 
-__version__ = '1.0.5'
+__version__ = '1.0.6'
 __author__ = 'Mori Benech'
 
 from .api import ConnectorManager, ConnectorAPI, ConnectorRemoteTool
