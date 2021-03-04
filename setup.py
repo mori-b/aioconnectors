60d4a5591e055e6854356d791102bccb6ab7b451
Mori Benech <mori@trapx.com>
2021-03-04 10:58:14 +0200

    version 1.0.37 	modified:   aioconnectors/__init__.py 	modified:   setup.py

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from setuptools import setup, find_packages
 from codecs import open
 from os import path
 
-VERSION = '1.0.36'
+VERSION = '1.0.37'
 
 here = path.abspath(path.dirname(__file__))
 # Get the long description from the README file
